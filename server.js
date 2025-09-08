23dd826e2fe85ad265d1f78bed12e7793cf231fa
Ju Williams <ju.williams00@gmail.com>
2025-09-08 19:53:37 +0530

    Update server.js to use environment variable for MongoDB URI

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ const app = express();
 app.use(express.json());
 
 // ===== MongoDB Connection =====
-mongoose.connect("mongodb+srv://juwilliams007_db_user:Ic%40ndoit1@learnvest-erp.0lg3fw2.mongodb.net/learnvest_erp?retryWrites=true&w=majority&appName=Learnvest-erp", {
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
@@ -21,7 +24,6 @@ app.use("/api/employees", employeeRoutes);
 const workLogRoutes = require("./routes/worklogs");
 app.use("/api/worklogs", workLogRoutes);
 
-
 // ===== Test Route =====
 app.get("/", (req, res) => {
   res.send("Learnvest ERP API is running...");
@@ -32,4 +34,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
