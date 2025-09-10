e5d4259cffdc4668892a9cfcb1c5146304b01f67
juwilliams007 <ju.williams00@gmail.com>
2025-09-10 05:41:56 -0400

    Temp rename to Employee.js

---
+++ models/Employee.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const employeeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  position: {
+    type: String,
+    required: true,
+  },
+  dateHired: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// The first argument "Employee" = collection will be "employees"
+module.exports = mongoose.model("Employee", employeeSchema);
